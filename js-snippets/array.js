f3fc0522d9feeb4121025eee8231c4cc13e0b365
sayhelloelijah <say@helloelijah.com>
2019-11-22 22:05:10 -0600

    add more snippets

--- js-snippets/array.js
+++ js-snippets/array.js
@@ -1,9 +1,10 @@
 // jshint esversion: 6
 // all easily use array.every
-let all = (arr, func) => arr.every(func);
+const all = (arr, func) => arr.every(func);
 // Test: console.log(all([2, 4], item => item % 2 == 0));
 
-let some = (arr, func) => arr.some(func);
+const some = (arr, func) => arr.some(func);
+const addAll = (arr) => arr.reduce((a, b) => a + b);
 
 // use reduce to count the occurences of a value
 const countOccurrences = (arr, val) => arr.reduce((a, v) => v === val ? a + 1 : a, 0);
--- js-snippets/math.js
+++ js-snippets/math.js
@@ -4,3 +4,5 @@
 const isEven = number => number % 2 === 0;
 // check if a number is odd
 const isOdd = number => number % 2 === 1;
+// check if multiple of number
+const isMultipleOf = (multiple, value) => value % multiple === 0;
