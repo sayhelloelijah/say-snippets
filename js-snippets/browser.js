f322a18548e816f3a91c7b0a7e24230866fc6fa1
Elijah Williams <elijah.williams@newbalance.com>
2020-02-21 11:51:34 -0600

    add sticky snippet

--- js-snippets/browser.js
+++ js-snippets/browser.js
@@ -6,11 +6,35 @@
  * @param {Boolean} asLink True: simulate click event; False: simulate HTTP Redirect
  */
 const redirect = (url, asLink = true) => asLink ? (window.location.href = url) : window.location.replace(url);
-redirect('https://google.com');
+//redirect('https://google.com');
 
 /**
  * HTTPS Redirect Function
  * If user isn't on HTTPS, redirect user to HTTPS
  */
-const httpsRedirect = () => {if (location.protocol !== 'https:') location.replace('https://' + location.href.split('//')[1])}};
-httpsRedirect(); // If you are on http://mydomain.com, you are redirected to https://mydomain.com
+const httpsRedirect = () => {if (location.protocol !== 'https:') location.replace('https://' + location.href.split('//')[1])};
+//httpsRedirect(); If you are on http://mydomain.com, you are redirected to https://mydomain.com
+
+/**
+ * Sticky Position
+ * Fix Element to top of the page. For use when "sticky position" is unsupported
+ * Add 'data-scroll' to any anchor element for scrolling effect
+ * 
+ * @param {Object} e    The event object
+ */
+function sticky(e) {
+    e.preventDefault();
+    let windowSize = window.outerWidth,
+        position = document.querySelector(this.getAttribute('href')).offsetTop - 50,
+        buffer = document.querySelector('.navbar') ? document.querySelector('.navbar').offsetHeight : 50;
+
+    windowSize < 768 ? false : position -= buffer; // cancel on mobile
+    window.scrollTo({
+        top: position,
+        behavior: 'smooth'
+    });
+};
+// Loop through all elements with 'data-scroll' apply click event
+// document.querySelectorAll('[data-scroll]').forEach(el => {
+//     el.addEventListener('click', sticky);
+// });
